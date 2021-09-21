be8084494e9b56ecd9986e58ca15e24a9d3e9d00
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-09-21 22:48:51 +0530

    [akhila]Refactor:added the getbook details in page

--- Js/getbooks.js
+++ Js/getbooks.js
@@ -1,21 +1,37 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    GetNotes();
+    GetBooks();
   });
+  const Baseurl2 =  "https://new-bookstore-backend.herokuapp.com/";
   let BooksList = [];
   function GetBooks() {
-    document.getElementById("main-section").style.display = "block";
-    makePromiseCall("GET", `${Baseurl}bookstore_user​/get​/book`, true, {}, true)
+   
+    makePromiseCall("GET", `${Baseurl2}bookstore_user/get/book`, true, {}, true)
       .then(res => {
-        console.log(JSON.parse(res).data.result);
+        console.log(JSON.parse(res).result);
         var nHTML = '';
-        BooksList = JSON.parse(res).data.result;
+        BooksList = JSON.parse(res).result;        
+        //document.getElementById("main-list").innerHTML = nHTML;
         for (var i = 0; i < BooksList.length; i++) {
-           var Add_item = "Additem" + BooksList[i]._id;
-           var Wishlist = "wishlist" + BooksList[i]._id;
-           nHTML += '<div class'
+          nHTML += `<div class="main-section">
+          <div class="sub-section">
+              <div class="sec">
+          <div class="books">
+              <img class="img" src = "../Assests/book2.png" alt="" >
+              </div>
+              <div class="book-text">
+              <h1 class="main-text">Don't Make Me Think</h1>
+              <h class="sub-text">by steve krug</h>
+              <button class="rating">4.5</button>
+              <div class="cost">RS.1500</div>
+          </div>
+          </div>
+          </div>
+          </div>`
         }
-            document.getElementById("main-section").innerHTML = nHTML;
+       // console.log(nHTML)
+        document.getElementById("main-list").innerHTML = nHTML;
         }) 
+        //console.log(nHTML)
         .catch((err) => {
           console.log(err);
         })
